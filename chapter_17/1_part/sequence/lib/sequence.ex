23c4063cb47d105f0833063ec063a2761d04beea
serjpetrenko <b.g.lupus@gmail.com>
2022-03-20 21:36:17 +0200

    Finish chapter_17

--- chapter_17/1_part/sequence/lib/sequence.ex
+++ chapter_17/1_part/sequence/lib/sequence.ex
@@ -1,2 +1,15 @@
 defmodule Sequence do
+  @server Sequence.Server
+
+  def start_link(list) do
+    GenServer.start_link(@server, list, name: @server)
+  end
+
+  def pop do
+    GenServer.call(@server, :pop)
+  end
+
+  def push(value) do
+    GenServer.cast(@server, {:push, value})
+  end
 end
--- chapter_17/1_part/sequence/lib/sequence/server.ex
+++ chapter_17/1_part/sequence/lib/sequence/server.ex
@@ -5,20 +5,16 @@ defmodule Sequence.Server do
     {:ok, arg}
   end
 
-  def handle_call(:next_number, _from, current_number) do
-    {:reply, current_number, current_number + 1}
-  end
-
   def handle_call(:pop, _from, [head | tail]) do
     {:reply, head, tail}
   end
 
-  def handle_cast({:increment_number, delta}, current_number) do
-    {:noreply, current_number + delta}
-  end
-
   def handle_cast({:push, value}, list) do
     {:noreply, [value | list]}
+  end
+
+  def terminate(reason, state) do
+    IO.puts("The reason is #{inspect reason} with state #{inspect state}")
   end
 
   def format_status(_reason, [_pdict, state]) do
